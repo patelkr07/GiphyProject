dd4f9f22061b8f9b6a6962978de590ad004b95cf
Kunal <patelkr07@gmail.com>
2019-04-03 21:55:08 -0500

    starting over

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 
 
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=&api_key=ke46yN1pXZHc3sQU577y1Y4KoyJBtIZy&limit=10"
+var queryURL = "http://api.giphy.com/v1/gifs/search?q=&api_key=ke46yN1pXZHc3sQU577y1Y4KoyJBtIZy&limit=10";
 
 
 $.ajax({
@@ -10,8 +10,20 @@ $.ajax({
     console.log(response);
   });
 
-  var queries = [BMW, Ford, Lincoln, Mercedes-Benz, Audi, Toyota, Volvo, Kia, Hyundai, Nissan, Infiniti];
+
+
+  var queries = ["BMW", "Ford", "Lincoln", "Mercedes-Benz", "Audi", "Toyota", "Volvo", "Kia", "Hyundai", "Nissan", "Infiniti"];
+
+  function renderButtons() {
+    $("#buttons-view").empty()
+  };
 
   var i; 
 
-  for 
+  for (i=0; i < queries.length; i++) {
+  
+  var b = $("<button>");
+    b.text(queries[i]);
+
+    $("#")
+  }
