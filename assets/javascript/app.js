1cd1a005a01565ec69416d39ba452e9b6489d265
Kunal <patelkr07@gmail.com>
2019-04-03 22:29:26 -0500

    adding functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,67 @@
 
+var queries = ["BMW", "Ford", "Lincoln", "Audi", "Toyota", "Volvo", "Hyundai", "Nissan", "Infiniti"];
 
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=&api_key=ke46yN1pXZHc3sQU577y1Y4KoyJBtIZy&limit=10";
+function displayCarInfo() {
+
+  var car = $(this).attr("placeholder");
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=&api_key=ke46yN1pXZHc3sQU577y1Y4KoyJBtIZy&limit=10";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+
+    var carDiv = $("<div class='car'>");
+
+    var rating = response.Rated;
+
+    var ratingDisplay = $("<p>").text("Rating: " + rating);
+
+    carDiv.append(ratingDisplay);
+
+    var title = response.Title;
+
+    var titleDisplay = $("<p>").text("Title: " + title);
+
+    carDiv.append(titleDisplay);
+
+    $("#car-view").prepend(carDiv);
+  });
+
+  }
+
+  function renderButtons() {
+
+    $("#car-view").empty();
+
+    for (var i = 0; i < queries.length; i++) {
+      
+      var a = $("<button">);
+
+      a.text(queries[i]);
+
+      const button2 = $("<button>").addClass('car-btn')
+                      .text(queries[i]);
+
+            $("#car-view").append(a);
+    }
+
+  }
+
+  $("#add-car").on("click", function(event) {
+    event.preventDefault();
+    var car = $("#car-input")
+  }
+
+    })
+  
+  }
+
+  )
+}
+}
+
+var queryURL = "
 
 
 $.ajax({
